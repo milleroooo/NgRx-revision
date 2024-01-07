56f93cf6478cf2239f89dae2e38dad3f725bee0e
milleroooo <maykmiller03@gmail.com>
2024-01-07 19:50:32 +0100

    #1 - Project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CounterOutputComponent, CounterControlsComponent],
 })
-export class AppComponent {
-  title = 'NgRx-revision';
-}
+export class AppComponent {}
